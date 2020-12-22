e680036dffeeeb20f16667a7e544de5fe49cebc7
rokon4eg <32700236+rokon4eg@users.noreply.github.com>
2020-12-22 14:56:32 +0300

    knot.count

--- screen.py
+++ screen.py
@@ -220,7 +220,7 @@ def draw_points(points, style="points", width=3, color=(255, 255, 255)):
                                (p.x, p.y), width)
 
 
-def draw_help():
+def draw_help(steps):
     """функция отрисовки экрана справки программы"""
     gameDisplay.fill((50, 50, 50))
     font1 = pygame.font.SysFont("courier", 24)
@@ -241,98 +241,6 @@ def draw_help():
             text[0], True, (128, 128, 255)), (100, 100 + 30 * i))
         gameDisplay.blit(font2.render(
             text[1], True, (128, 128, 255)), (200, 100 + 30 * i))
-
-
-# =======================================================================================
-# Функции, отвечающие за расчет сглаживания ломаной
-# =======================================================================================
-def get_point(points, alpha, deg=None):
-    if deg is None:
-        deg = len(points) - 1
-    if deg == 0:
-        return points[0]
-    return add(mul(points[deg], alpha), mul(get_point(points, alpha, deg - 1), 1 - alpha))
-
-
-def get_points(base_points, count):
-    alpha = 1 / count
-    res = []
-    for i in range(count):
-        res.append(get_point(base_points, i * alpha))
-    return res
-
-
-def get_knot(points, count):
-    if len(points) < 3:
-        return []
-    res = []
-    for i in range(-2, len(points) - 2):
-        ptn = []
-        ptn.append(mul(add(points[i], points[i + 1]), 0.5))
-        ptn.append(points[i + 1])
-        ptn.append(mul(add(points[i + 1], points[i + 2]), 0.5))
-
-        res.extend(get_points(ptn, count))
-    return res
-
-
-def set_points(points, speeds):
-    """функция перерасчета координат опорных точек"""
-    for p in range(len(points)):
-        points[p] = add(points[p], speeds[p])
-        if points[p][0] > SCREEN_DIM[0] or points[p][0] < 0:
-            speeds[p] = (- speeds[p][0], speeds[p][1])
-        if points[p][1] > SCREEN_DIM[1] or points[p][1] < 0:
-            speeds[p] = (speeds[p][0], -speeds[p][1])
-
-
-# =======================================================================================
-# Основная программа
-# =======================================================================================
-def run_functional_version():
-    global working, pause, show_help, hue, steps
-    points = []
-    speeds = []
-
-    while working:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                working = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    working = False
-                if event.key == pygame.K_r:
-                    points = []
-                    speeds = []
-                if event.key == pygame.K_p:
-                    pause = not pause
-                if event.key == pygame.K_KP_PLUS:
-                    steps += 1
-                if event.key == pygame.K_KP_MINUS:
-                    steps -= 1 if steps > 1 else 0
-                if event.key == pygame.K_F1:
-                    show_help = not show_help
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button in (1, 4):
-                    points.append(event.pos)
-                    speeds.append((random.random() * 2, random.random() * 2))
-                else:
-                    points.pop()
-                    speeds.pop()
-
-        gameDisplay.fill((0, 0, 0))
-        hue = (hue + 1) % 360
-        color.hsla = (hue, 100, 50, 100)
-        draw_points(points)
-        draw_points(get_knot(points, steps), "line", 3, color)
-        if not pause:
-            set_points(points, speeds)
-        if show_help:
-            draw_help()
-
-        pygame.display.flip()
-
 
 def run_class_version():
     global working, pause, steps, show_help, hue
@@ -358,9 +266,9 @@ def run_class_version():
                 if event.key == pygame.K_p:
                     pause = not pause
                 if event.key == pygame.K_KP_PLUS:
-                    steps += 1
+                    knot.count += 1
                 if event.key == pygame.K_KP_MINUS:
-                    steps -= 1 if steps > 1 else 0
+                    knot.count -= 1 if knot.count > 1 else 0
                 if event.key == pygame.K_F1:
                     show_help = not show_help
 
@@ -388,7 +296,7 @@ def run_class_version():
         if not pause:
             knot.set_points()
         if show_help:
-            draw_help()
+            draw_help(knot.count)
 
         pygame.display.flip()
 
